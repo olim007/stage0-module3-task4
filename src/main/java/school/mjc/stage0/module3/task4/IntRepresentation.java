85bfdea876ce43204717aceb8968ff9a4e734207
Olimzhoni Narzullozoda <olimzhon01@mail.ru>
2023-06-23 00:06:47 +0300

    solved

--- src/main/java/school/mjc/stage0/module3/task4/IntRepresentation.java
+++ src/main/java/school/mjc/stage0/module3/task4/IntRepresentation.java
@@ -2,5 +2,10 @@ package school.mjc.stage0.module3.task4;
 
 public class IntRepresentation {
     public static void main(String[] args) {
+//        System.out.println((int)'c' + " " + (int)'h' + " " + (int)'a' + " " + (int)'r');
+        System.out.print((char)99);
+        System.out.print((char)104);
+        System.out.print((char)97);
+        System.out.print((char)114);
     }
 }
